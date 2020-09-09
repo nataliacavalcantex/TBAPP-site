e63811a865e9b1fcd6b685fc56260a903c84e4d5
nataliacavalcantex <nataliacxavier1@gmail.com>
2020-09-09 13:46:51 -0400

    feat:update_user_and_profilepage

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -31,16 +31,46 @@ class UserController{
     }
     async index(req,res){
         console.log(req.userId)
-        // const user= await User.findOne({
-        //     where:{
-        //         id:req.userId
-        //     }
-        // })
-
-        // if(!user){
-        //     return res.status(404).json({error:"User not found"})
+        const user= await User.findOne({
+            where:{
+                id:req.userId
+            }
+        })
+        if(!user){
+            return res.status(404).json({error:"User not found"})
+        }
+        return res.json(user)
+    }
+    async update(req,res){
+        const schema=Yup.object().shape({
+            name:Yup.string(),
+            email:Yup.string().email(),
+            oldPassword:Yup.string().min(6),
+            password:Yup.string().min(6).when('oldPassword',(oldPassword,field)=> oldPassword ? field.required() : field),
+            confirmPassword:Yup.string().when('password',(password,field)=> password ? field.required().oneOf([Yup.ref('password')]) : field)
+        })
+        console.log(req.body)
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error:'Validation failed'})
+        }
+        const {email,oldPassword} =req.body
+        const user= await User.findByPk(req.userId)
+        if(email && email !== user.email){
+            const userExists= await User.findOne({where:{email}})
+            if(userExists){
+                return res.status(400).json({error:'User already exist'})
+            }
+        }
+        // if(oldPassword && !(await user.checkPassword(oldPassword))){
+        //     return res.status(401).json({error:'Password does not mach'})
         // }
-        // return res.json(user)
+        const {id,name} = await user.update(req.body)
+        return res.json({
+            id,
+            name,
+            email,
+           
+        })
     }
 
 }
